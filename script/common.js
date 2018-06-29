d23d5317d241cedeae0885167487058c9194d0e0
KimDaeHyung <40633069+KimDaeHyung@users.noreply.github.com>
2018-06-30 00:16:09 +0900

    Update common.js

--- script/common.js
+++ script/common.js
@@ -44,16 +44,28 @@
 	//////////////////////////////////////////////////////
 	
 	//액션 및 이벤트
+
+	/* 글읽기 */
 	function gfContentList(){
+		var rand = Number(Math.floor(Math.random() * 8));
+		$("#frmRead user").val(gUserArray[rand]);
+		var sAction = "/read";
+		var fnCallback = gfContentListCallback;
+		gfAjaxCallWithForm(sAction,$('#frmRead'),fnCallback,"POST")
+	}
+	
+	
+	function gfContentListCallback(data){
+		
 		$("div[id='contentList']").empty();
 		
-		for ( var x = 0 ; x < gContentArray.length ; x++ ){
+		for ( var x = 0 ; x < data.length ; x++ ){
 			/*
 			var strHtml	= '<ol class="breadcrumb">' 
 						+ '		<li class="breadcrumb-item active">' + gContentArray[x].content + '</li>'
 						+ '</ol>';
 			*/
-
+/*
 			var imgIdx=0;
 			for ( var y = 0 ; y < gUserArray.length ; y++ ){
 				if ( gUserArray[y] == gContentArray[x].user ){
@@ -61,27 +73,28 @@
 					break;
 				}
 			}
-			
+*/			
 			var strHtml	= '<div class="element tile-1 home calc bg-change">'
 						+ '	<table style="width: 100%;">'
 						+ '		<tr>'
 						+ '			<td>'
-						+ '				<h4 class="header icon-to-the-right">' + gContentArray[x].user + '</h4>'
+						+ '				<h4 class="header icon-to-the-right">' + data[x].account + '</h4>'
 						+ '			</td>'
 						+ '			<td  style="text-align:right; margin: 0px;">'
 						+ '				<img style="padding: 0px 0px 0px 0px; display: inline; max-height: 40px; max-width: 40px;" src="./images/user/' + imgIdx + '.png">'
 						+ '			</td>'
 						+ '		</tr>'
 						+ '	</table>'
-						+ '	<p class="preCon">' + gContentArray[x].content + '</p>'
+						+ '	<p class="preCon">' + data[x].data + '</p>'
 						+ '	<p></p>'
-						+ '	<p class="hint">'+ gContentArray[x].like + '명이 좋아합니다 핡핡~</p>'
+						+ '	<p class="hint">'+ data[x].voting + '명이 Voting 핡핡~</p>'
 						+ '	<div name="divStyle" ></div>'
 						+ '</div>';
 			
 			$("div[id='contentList']").append(strHtml);
 		}
 	}
+	
 	
 	function gfContentWriteAction(){
 		var contentObject = new Object();
